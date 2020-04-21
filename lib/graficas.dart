7f2753dcf517451276927eb2ba583809d8722070
01FC <javier.francisco01.09@gmail.com>
2020-04-21 08:28:24 -0500

    data table y graficas pay y barras

--- lib/graficas.dart
+++ lib/graficas.dart
@@ -23,7 +23,57 @@ class _GraficasState extends State<Graficas> {
   }
 
   Widget _simple(dynamic _colors) {
-    // TODO
+    var _chartElements = List<charts.Series<_ChartObject, String>>();
+    var chartData = widget.listadoDeDatos
+        .map(
+          (item) => _ChartObject(
+            xAxis: item["producto"],
+            yAxis: item["cantidad"],
+          ),
+        )
+        .toList();
+
+    _chartElements.add(
+      charts.Series<_ChartObject, String>(
+        id: "Grafica132",
+        data: chartData,
+        measureFn: (object, index) => object.yAxis,
+        domainFn: (object, index) => object.xAxis,
+        colorFn: (object, index) => _colors[index].shadeDefault,
+        labelAccessorFn: (object, index) => "${object.xAxis} : ${object.yAxis}",
+      ),
+    );
+
+    if (widget.showBarChart) {
+      return charts.BarChart(
+        _chartElements,
+        animate: true,
+        behaviors: [
+          charts.ChartTitle("Productos"),
+          charts.DatumLegend(desiredMaxRows: 2),
+        ],
+      );
+    } else {
+      return charts.PieChart(
+        _chartElements,
+        animate: true,
+        behaviors: [
+          charts.ChartTitle("Productos"),
+          charts.DatumLegend(
+            desiredMaxRows: 2,
+            position: charts.BehaviorPosition.bottom,
+          ),
+        ],
+        defaultRenderer: charts.ArcRendererConfig(
+          arcRatio: 0.6,
+          arcRendererDecorators: [
+            charts.ArcLabelDecorator(
+              labelPosition: charts.ArcLabelPosition.auto,
+            ),
+          ],
+        ),
+      );
+    }
   }
 }
 
--- lib/tablas.dart
+++ lib/tablas.dart
@@ -12,6 +12,7 @@ class Tablas extends StatefulWidget {
 class _TablasState extends State<Tablas> {
   var _columnas = List<DataColumn>();
   var _renglones = List<DataRow>();
+
   @override
   void initState() {
     _fillColumns();
@@ -21,9 +22,11 @@ class _TablasState extends State<Tablas> {
 
   @override
   Widget build(BuildContext context) {
+    // TODO: Ordenar, en la Column indicar alguna funcion para ordenar, onSort:
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: DataTable(
+        sortColumnIndex: 0,
         columns: _columnas,
         rows: _renglones,
       ),
@@ -32,11 +35,32 @@ class _TablasState extends State<Tablas> {
 
   // obtener nombre de columnas
   void _fillColumns() {
-    // TODO
+    _columnas = widget.listadoDeDatos[0].entries
+        .map(
+          (item) => DataColumn(
+            label: Text("${item.key}"),
+          ),
+        )
+        .toList();
   }
 
   // obtener datos de renglones
   void _fillRows() {
-    // TODO
+    for (var item in widget.listadoDeDatos) {
+      List<DataCell> celdas = item.entries
+          .map(
+            (entry) => DataCell(
+              entry.key == "color"
+                  ? Container(
+                      height: 8,
+                      width: 8,
+                      color: Color(int.parse("${entry.value}")),
+                    )
+                  : Text("${entry.value}"),
+            ),
+          )
+          .toList();
+      _renglones.add(DataRow(cells: celdas));
+    }
   }
 }
